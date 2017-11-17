77769038dc3b1950f92605f8dc9c06151bc88f7d
Nakamura Yuta <Nakamura Yuta>
2017-11-17 15:11:34 +0900

    updated tweet_neko_ramen.py

--- tweet_neko_ramen.py
+++ tweet_neko_ramen.py
@@ -3,19 +3,26 @@ import datetime
 import twy
 
 neko = NekoRamen() 
-if neko.check_update():
-    new_post = neko.get_latest_post()
-    if new_post.check_status() == OPEN:
-        tweet = u"本日営業\n" + new_post.content
-    elif new_post.check_status()  ==  CLOSED:
-        tweet = u"本日休業\n" + new_post.content
-    elif new_post.check_status()  == SOLD_OUT:
-        tweet = u"売り切れました\n" + new_post.content
+new_post = neko.get_latest_post()
 
-    if len(tweet) > 140:
-        tweet = tweet[:140] + "..."
-else:
-    tweet = "This is for debug use"+ str( datetime.datetime.now() )
-print(tweet)
+if new_post.check_status() == OPEN:
+    tweet = u"本日営業\n" + new_post.content
+elif new_post.check_status()  ==  CLOSED:
+    tweet = u"本日休業\n" + new_post.content
+elif new_post.check_status()  == SOLD_OUT:
+    tweet = u"売り切れました\n" + new_post.content
+
+if len(tweet) > 140:
+    tweet = tweet[:140] + "..."
+
 api = twy.api
-api.update_status(status=tweet)
+last_tweet = api.user_timeline()[0].text
+# when tweet is already posted, 
+print(last_tweet)
+print(tweet)
+
+if last_tweet[10]  == tweet[10]:
+    print("already tweeded")
+else:
+    print("new tweet") 
+    api.update_status(status=tweet)
